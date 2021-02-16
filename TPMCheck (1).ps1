afc656e48adf390c0ef63d7940af4cfb93194c8e
vikashhack1 <74226641+vikashhack1@users.noreply.github.com>
2021-02-16 16:27:23 +0530

    Add files via upload

---
+++ TPMCheck (1).ps1
@@ -0,0 +1,10 @@
+﻿$TestTPM =gc $Env:USERPROFILE\AppData\Local\Packages\PaloAltoNetworks.GlobalProtect_rn9aeerfb38dg\LocalState\DiagOutputDir\PanGPS.log |sls "error 0x80090016: Keyset does not exist"
+
+if ($TestTPM -match "error 0x80090016: Keyset does not exist")  # The term in brackets is a true/false value
+{
+    Write-Host "Device have TPM issue"
+ }
+else 
+{
+    Write-Host "TPM is okay on this device"
+}
